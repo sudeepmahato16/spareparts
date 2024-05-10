d8ea5a060abb29da481e4a4f6bb4ac93da76d0e8
sudeepmahato16 <sudeepmahato16@gmail.com>
2024-05-10 12:23:18 +0545

    implement error handling

--- server/app.ts
+++ server/app.ts
@@ -1,9 +1,12 @@
 import express from "express";
+import { errorHandler } from '@/controller/error';
 
 const app = express();
 
 app.get('/', (req, res, next) => {
   res.send("hello world!");
 })
+
+app.use(errorHandler);
 
 export default app;
